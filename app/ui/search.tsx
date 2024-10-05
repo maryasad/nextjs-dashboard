944f7ed246d3a351007886d99de573ad1a997898
Maryam <maryasad76@yahoo.com>
2024-10-05 17:39:08 +0200

    update

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -10,15 +11,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
+    const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
-  return (
+  }, 300);
+    return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
@@ -29,7 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-      />
+        defaultValue={searchParams.get('query')?.toString()}
+        />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
